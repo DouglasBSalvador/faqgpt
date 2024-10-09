dfe4af04eb8f4f530ccc602f0fe91b4ccae26110
DouglasBSalvador <douglasborota@hotmail.com>
2024-10-09 20:11:29 -0300

    entrega02(front): atualiza interface principal

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header style={{ padding: '10px', background: '#3a5bc6', color: '#ededed', textAlign: 'center' }}>
+            <h1>FaqGPT</h1>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/components/MainContent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainContent = () => {
+    return (
+        <main style={{ padding: '15px',  color: '#3a5bc6', textAlign: 'center'  }}>
+            <h2>Bem-vindo ao FaqGPT Gamer!</h2>
+            <p>Aqui você pode tirar suas duvidas sobre o mundo gamer!</p>
+        </main>
+    );
+};
+
+export default MainContent;
